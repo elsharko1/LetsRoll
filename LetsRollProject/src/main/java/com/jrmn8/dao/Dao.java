18b392deb44500ac03b29dd86e09ea5acc923d4c
Richard Ren <rren1952@gmail.com>
2017-08-11 15:21:31 -0400

    z

--- LetsRollProject/src/main/java/com/jrmn8/dao/Dao.java
+++ LetsRollProject/src/main/java/com/jrmn8/dao/Dao.java
@@ -3,15 +3,9 @@ package com.jrmn8.dao;
 import java.util.ArrayList;
 
 public interface Dao {
-/*    ArrayList<EventsDto> getEventList();
 
-    ArrayList<EventsDto> getEventByCity(String city);
+    // Create, Update, Read, Delete
+    // CURD
 
-    ArrayList<EventsDto> getEventByTitle(String titleKeyWord);
-
-    EventsDto getEventById(int eventId);
-
-    EventsDto attendEvent(int eventId, boolean b);
-
-    void createEvent(EventsDto event);*/
+    
 }
