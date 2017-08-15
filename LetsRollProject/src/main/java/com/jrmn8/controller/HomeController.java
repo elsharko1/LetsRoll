41a22498d25b1d52a42ffef6b7260a06cd547a5d
jennystpeters <jennystpeters@gmail.com>
2017-08-15 19:43:29 -0400

    deleted unused lines

--- LetsRollProject/src/main/java/com/jrmn8/controller/HomeController.java
+++ LetsRollProject/src/main/java/com/jrmn8/controller/HomeController.java
@@ -15,7 +15,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.hibernate.criterion.Restrictions;
 import org.hibernate.id.IdentifierGenerationException;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,13 +22,10 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import org.hibernate.*;
